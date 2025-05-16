8c12c7ea5ae23887bebdf36f9feeea1e321ca873
Nikola <niks.manov@gmail.com>
2025-05-16 21:27:57 +0300

    Reverse row-col in cube side

--- RubikCube.UI/src/components/cube/cubeSide.tsx
+++ RubikCube.UI/src/components/cube/cubeSide.tsx
@@ -2,7 +2,7 @@ import * as types from '../../common/types';
 export default function CubeSide({ side, name }: { side: types.CubeSide, name: string }) {
     return (<div className={`side ${name}`}>
         {side.facelets.map((f, i) => {
-            return <div key={i} data-col={f.column} data-row={f.row} className={`sticker ${f.color.toLowerCase()}`} />
+            return <div key={i} data-row={f.row} data-col={f.column} className={`sticker ${f.color.toLowerCase()}`} />
         })}
     </div>);
 }
